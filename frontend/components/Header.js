2ed926ad3e60b491f49600096389dafccb719c05
german nunez <german90nunez@gmail.com>
2020-09-22 23:16:43 -0700

    mobile nav bar and desktop

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,64 @@
+import Link from 'next/link'
+import Logo from '../images/np_logo.png'
+import styles from '../styles/Header.module.css'
+import { useState } from 'react'
 const Header = () => {
+    const [ openNav, setOpenNav ] = useState(false)
+
+    const navbarOpen = () => {
+        setOpenNav(!openNav)
+    }
     return(
-        <h1>I r header</h1>
+        <header className={styles.header}>
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src={Logo}/>
+                <p>German Nunez</p>
+            </div>
+            <nav className={styles.nav}>
+                <ul>
+                    <Link href="/blog">
+                        <a>Blog</a>
+                    </Link>
+                    <Link href="/blog">
+                        <a>Projects</a>
+                    </Link>
+                    <Link href="/blog">
+                        <a>About Me</a>
+                    </Link>
+                    <Link href="/blog">
+                        <a>Contact</a>
+                    </Link>
+                </ul>
+            </nav>
+            <div onClick={navbarOpen} className={styles.hamburgerContainer}>
+                <div className={styles.hamburger}/>
+                <div className={styles.hamburger}/>
+                <div className={styles.hamburger}/>
+            </div>
+
+            <MobileOverLay openNav={openNav}/>
+        </header>
+    )
+}
+
+const MobileOverLay = ({openNav}) => {
+    return (
+        <div className={openNav ? styles.mobileOverlay : null}>
+            <ul>
+                <Link href="/blog">
+                    <a>Blog</a>
+                </Link>
+                <Link href="/blog">
+                    <a>Projects</a>
+                </Link>
+                <Link href="/blog">
+                    <a>About Me</a>
+                </Link>
+                <Link href="/blog">
+                    <a>Contact</a>
+                </Link>
+            </ul>
+        </div>
     )
 }
 
