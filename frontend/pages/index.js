c1ad531fc9fc6c261dd48da173be4114ae7d488f
german nunez <german90nunez@gmail.com>
2020-09-25 18:50:04 -0700

    service cards rough sketch mobile responsive

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import rocket from '../images/space_ship_icon.png'
 import axios from 'axios'
+import {AiFillApi} from 'react-icons/ai'
+import {CgWebsite} from 'react-icons/cg'
+import {ImDatabase} from 'react-icons/im'
 export default function Home() {
   React.useEffect(() => {
     axios.get('http://localhost:1337/employments')
@@ -13,12 +16,39 @@ export default function Home() {
       })
   }, [])
   return (
-    <div className={styles.container}>
-      <div className={styles.landingHeading}>
-        <img src={rocket} alt="rocket blasting off icon"/>
-        <div>
-          <h2>Software Engineer</h2>
-          <p>Let's Make it Happen</p>
+    <div>
+
+      <div className={styles.homeContainer}>
+        <div className={styles.landingHeading}>
+          <img src={rocket} alt="rocket blasting off icon"/>
+          <div>
+            <h2>Software Engineer</h2>
+            <p>Let's Make it Happen</p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.services}>
+        <div className={styles.serviceCard}>
+          <div>
+            <h2>API Development</h2>
+            <AiFillApi className={styles.serviceIcon} />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+        <div className={styles.serviceCard}>
+          <div>
+            <h2>Web App Development</h2>
+            <CgWebsite className={styles.serviceIcon} />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
+        </div>
+        <div className={styles.serviceCard}>
+          <div>
+            <h2>Database Development</h2>
+            <ImDatabase className={styles.serviceIcon} />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
         </div>
       </div>
     </div>
