58a4aeaabde2e11074833dbb17090ccdb9a7fee3
german nunez <german90nunez@gmail.com>
2020-09-27 21:55:04 -0700

    footer with sm links

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -1,9 +1,16 @@
 import styles from '../styles/Footer.module.css'
+import {AiOutlineLinkedin, AiFillGithub} from 'react-icons/ai'
 
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <p>&#169; German Nunez 2020</p>
+            <div>
+                <p>&#169; German Nunez 2020</p>
+            </div>
+            <div className={styles.footerIcons}>
+                <AiOutlineLinkedin className={styles.icon} />
+                <AiFillGithub className={styles.icon}/>
+            </div>
         </div>
     )
 }
